942f762c6f98006349a132645de02dde9598a46b
Pavel Kozhevnikov <dematium@gmail.com>
2020-08-15 02:45:27 +0300

    tinyxml configs

--- Project/Project/main.cpp
+++ Project/Project/main.cpp
@@ -1,8 +1,44 @@
 #include <SFML/Graphics.hpp>
+#include <tinyxml.h>
+#include <tinystr.h>
+#include <string>
+
+struct WindowSettings
+{
+    WindowSettings() = default;
+
+    WindowSettings(int w, int h, const std::string& name)
+    : w(w)
+    , h(h)
+    , name(name)
+    {}
+
+    int w = 400;
+    int h = 400;
+    std::string name = "Untitled";
+};
+
+
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    WindowSettings windowSettings;
+
+    TiXmlDocument doc("config\\window.xml");
+    doc.LoadFile();
+	TiXmlHandle hDoc(&doc);
+	TiXmlElement* pElem;
+	TiXmlHandle hRoot(0);
+
+    pElem = hDoc.FirstChildElement().Element();
+    hRoot = TiXmlHandle(pElem);
+
+	TiXmlElement* pWindowNode = hRoot.FirstChild("Windows").FirstChild().Element();
+    windowSettings.name = pWindowNode->Attribute("name");
+    pWindowNode->QueryIntAttribute("w", &windowSettings.w);
+    pWindowNode->QueryIntAttribute("h", &windowSettings.h);
+
+    sf::RenderWindow window(sf::VideoMode(windowSettings.w, windowSettings.h), windowSettings.name);
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
